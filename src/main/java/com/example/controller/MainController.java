3ccd46828f76a28ce8a2624b6627c0a31d9414b8
최혜근 <@DESKTOP-CT2B6I5>
2021-04-30 11:07:24 +0900

    c

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -3,9 +3,7 @@ package com.example.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.websocket.server.PathParam;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/example/dao/MyDao.java
+++ src/main/java/com/example/dao/MyDao.java
@@ -1,6 +1,8 @@
 package com.example.dao;
 
 public class MyDao {
+	String name;
+	
 	public String test(String data) {
 		return data;
 	}
